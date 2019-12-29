2d252bf9d92ee8712926c4e6b622c12cb5733330
Eric Ito <eric.ito@gmail.com>
2019-12-28 19:02:34 -0800

    finish renames

---
+++ ItoKit/ItoKit.h
@@ -0,0 +1,19 @@
+//
+//  ItoKit.h
+//  ItoKit
+//
+//  Created by Eric Ito on 12/28/19.
+//  Copyright © 2019 Eric Ito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ItoKit.
+FOUNDATION_EXPORT double ItoKitVersionNumber;
+
+//! Project version string for ItoKit.
+FOUNDATION_EXPORT const unsigned char ItoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ItoKit/PublicHeader.h>
+
+
